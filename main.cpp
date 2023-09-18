747ffc5ee28280c38dae6a71af416bfff5494ceb
LeCorey <125359729+LeCorey@users.noreply.github.com>
2023-09-17 20:27:09 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,89 +1,60 @@
 //
-//  main.cpp
-//  ProgrammingProject2
-//
-//  Created by LeCorey Johnson on 9/8/23.
-//
 
+// main.cpp
+// Programming Project 1
+//
+// Created by LeCorey Johnson on 8/27/23.
+//
 #include <iostream>
 #include <fstream>
-#include <queue>
+#include <list>
 #include <string>
-
-using namespace std;
-
-class Shooter {
-private:
-    string name;
-    int bulletCount;
-    int initialBulletCount;
-
-public:
-    Shooter(string name, int bullet) : name(name), bulletCount(bullet), initialBulletCount(bullet) {}
-
-    void shooting() {
-        if (bulletCount > 0) {
-            bulletCount--;
-        }
-    }
-
-    string getName() {
-        return name;
-    }
-
-    int getQuantity() {
-        return bulletCount;
-    }
-
-    void display() {
-        cout << "Shooter's Name: " << name << ", Remaining Bullets: " << bulletCount << endl;
-    }
-
-    bool isComplete() {
-        return bulletCount == 0;
-    }
-
-    void reload() {
-        bulletCount = initialBulletCount;
-    }
+#include <sstream>
+// structure to store employee information
+struct Employee {
+int employeeId;
+std::string firstName;
+std::string lastName;
+std::string phoneNumber;
+std::string emailAddress;
+double salary;
 };
-
-int main() {
-    queue<Shooter> shootingRange;
-
-    ifstream inputFile("Week3-bulletCount.txt");
-    if (!inputFile) {
-        cerr << "Error: Unable to open input file." << endl;
-        return 1;
-    }
-
-    // Read shooter information from the file and create shooter objects
-    string name;
-    int bulletCount;
-    while (inputFile >> name >> bulletCount) {
-        Shooter shooter(name, bulletCount);
-        shootingRange.push(shooter);
-        
-        // Display status information
-        cout << "Shooter: " << shooter.getName() << " added to the queue. Remaining Bullets: " << shooter.getQuantity() << endl;
-    }
-
-    inputFile.close();
-
-    while (!shootingRange.empty()) {
-        Shooter shooter = shootingRange.front();
-        shootingRange.pop();
-
-        shooter.shooting();
-
-        if (shooter.isComplete()) {
-            cout << "Shooter with Name=" << shooter.getName() << " is completed." << endl;
-        } else {
-            cout << "Shooter with Name=" << shooter.getName() << " needs to shoot " << shooter.getQuantity() << " more bullets." << endl;
-            shootingRange.push(shooter);
-            cout << "Shooter Name=" << shooter.getName() << " is sent back to the queue. Remaining bullets: " << shooter.getQuantity() << endl;
-        }
-    }
-
-    return 0;
+// comparing employees based on ID
+bool compareById(const Employee& emp1, const Employee& emp2) {
+return emp1.employeeId < emp2.employeeId;
 }
+int main() {
+std::list<Employee> employees;
+std::ifstream inputFile("Week 1-employeeDataset_Project 1 (1).txt");
+if (!inputFile.is_open()) {
+std::cerr << "Failed to open the input file." << std::endl;
+return 1;
+}
+std::string line;
+while (std::getline(inputFile, line)) {
+Employee emp;
+std::istringstream iss(line);
+if (!(iss >> emp.employeeId >> emp.firstName >> emp.lastName >>
+emp.phoneNumber >> emp.emailAddress >> emp.salary)) {
+std::cerr << "Error reading employee data from line: " << line <<
+std::endl;
+continue;
+}
+employees.insert(std::upper_bound(employees.begin(), employees.end(), emp,
+compareById), emp);
+}
+inputFile.close();
+// Display the sorted employee list
+    for (const Employee& emp : employees) {
+        std::cout << "Employee ID: " << emp.employeeId << "\n"
+        << "First Name: " << emp.firstName << "\n"
+        << "Last Name: " << emp.lastName << "\n"
+        << "Phone Number: " << emp.phoneNumber << "\n"
+        << "Email Address: " << emp.emailAddress << "\n"
+        
+        
+        << "Salary: " << emp.salary << "\n"
+        << "-------\n";
+        }
+        return 0;
+        }
