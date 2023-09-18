e88ca4aac7a38bb4270805ba2cea63697cec4f00
LeCorey <125359729+LeCorey@users.noreply.github.com>
2023-09-17 20:26:25 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,94 +1,89 @@
-
+//
+//  main.cpp
+//  ProgrammingProject2
+//
+//  Created by LeCorey Johnson on 9/8/23.
 //
 
 #include <iostream>
+#include <fstream>
+#include <queue>
+#include <string>
 
-const int NUM_MONTHS = 12;
-const int MIN_TEMP = -100;
-const int MAX_TEMP = 140;
+using namespace std;
 
-struct WeatherData {
-    double totalRainfall;
-    double highTemperature;
-    double lowTemperature;
-    double averageTemperature;
-    
+class Shooter {
+private:
+    string name;
+    int bulletCount;
+    int initialBulletCount;
+
+public:
+    Shooter(string name, int bullet) : name(name), bulletCount(bullet), initialBulletCount(bullet) {}
+
+    void shooting() {
+        if (bulletCount > 0) {
+            bulletCount--;
+        }
+    }
+
+    string getName() {
+        return name;
+    }
+
+    int getQuantity() {
+        return bulletCount;
+    }
+
+    void display() {
+        cout << "Shooter's Name: " << name << ", Remaining Bullets: " << bulletCount << endl;
+    }
+
+    bool isComplete() {
+        return bulletCount == 0;
+    }
+
+    void reload() {
+        bulletCount = initialBulletCount;
+    }
 };
 
-void inputData(WeatherData& data, int month) {
-    std::cout << "Enter weather data for the Month" << month + 1 << ":\n";
-    
-    std::cout <<"Total rainfall: ";
-    std::cin >> data.totalRainfall;
-    
-    std::cout <<"High Temperature: ";
-    std::cin >> data.highTemperature;
-    
-    std::cout << "Low temperature: ";
-    std::cin >> data.lowTemperature;
-    
-    
-    data.averageTemperature = (data.highTemperature + data.lowTemperature) / 2;
-    
-}
-
-void validateTemperature(double& temperature) {
-    while (temperature < MIN_TEMP || temperature > MAX_TEMP) {
-        std::cout << "Temperature must beb between -100 and 140 degrees F. Please re-enter: ";
-        std::cin >> temperature;
-        
-    }
-}
-
-void calculateStatistics(WeatherData data[], double& averageMonthlyRainfall, double& totalYearlyRainfall, double& highestTemperature, int& highestTemperatureMonth, double& lowestTemperature, int& lowestTemperatureMonth, double& averageTemperature) {
-    
-    averageMonthlyRainfall = 0.0;
-    totalYearlyRainfall = 0.0;
-    highestTemperature = data[0].highTemperature;
-    highestTemperatureMonth = 0;
-    lowestTemperature = data[0].lowTemperature;
-    lowestTemperatureMonth = 0;
-    averageTemperature = 0.0;
-    
-    for (int i = 0; i < NUM_MONTHS; i++) {
-        averageMonthlyRainfall += data[i].totalRainfall;
-        totalYearlyRainfall += data[i].totalRainfall;
-        averageTemperature += data[i].averageTemperature;
-        
-        if (data[i].highTemperature > highestTemperature){
-            highestTemperature = data[i].highTemperature;
-            highestTemperatureMonth = i;
-            
-        }
-        
-        if (data[i].lowTemperature < lowestTemperature) {
-            lowestTemperature = data[i].lowTemperature;
-            lowestTemperatureMonth = i;
-        }
-    }
-    
-    averageMonthlyRainfall /= NUM_MONTHS;
-    averageTemperature /= NUM_MONTHS;
-}
-
 int main() {
-    WeatherData monthlyData[NUM_MONTHS];
-    double averageMonthlyRainfall, totalYearlyRainfall, highestTemperature, lowestTemperature, averageTemperature;
-    int highestTemperatureMonth, lowestTemperatureMonth;
-    
-    for (int i = 0; i < NUM_MONTHS; i++) {
-        inputData(monthlyData[i], i);
-        
+    queue<Shooter> shootingRange;
+
+    ifstream inputFile("Week3-bulletCount.txt");
+    if (!inputFile) {
+        cerr << "Error: Unable to open input file." << endl;
+        return 1;
     }
-    
-    calculateStatistics(monthlyData, averageMonthlyRainfall, totalYearlyRainfall, highestTemperature, highestTemperatureMonth, lowestTemperature, lowestTemperatureMonth, averageTemperature);
-    
-    std::cout << "\nYearly Weather Stats: \n";
-    std::cout << "Average monthly rainfall: " <<averageMonthlyRainfall << "\n";
-    std::cout << "Total Rain for the year: " << totalYearlyRainfall << "\n";
-    std::cout << "Highest Temp: " << highestTemperature << " (Month " << highestTemperatureMonth + 1 << ")\n";
-    std::cout << "Lowest Temp: " << lowestTemperature << " (Month " << lowestTemperatureMonth + 1 << ")\n";
-    std::cout << "Average of Monthly Average Temps: " << averageTemperature << std::endl;
-    
+
+    // Read shooter information from the file and create shooter objects
+    string name;
+    int bulletCount;
+    while (inputFile >> name >> bulletCount) {
+        Shooter shooter(name, bulletCount);
+        shootingRange.push(shooter);
+        
+        // Display status information
+        cout << "Shooter: " << shooter.getName() << " added to the queue. Remaining Bullets: " << shooter.getQuantity() << endl;
+    }
+
+    inputFile.close();
+
+    while (!shootingRange.empty()) {
+        Shooter shooter = shootingRange.front();
+        shootingRange.pop();
+
+        shooter.shooting();
+
+        if (shooter.isComplete()) {
+            cout << "Shooter with Name=" << shooter.getName() << " is completed." << endl;
+        } else {
+            cout << "Shooter with Name=" << shooter.getName() << " needs to shoot " << shooter.getQuantity() << " more bullets." << endl;
+            shootingRange.push(shooter);
+            cout << "Shooter Name=" << shooter.getName() << " is sent back to the queue. Remaining bullets: " << shooter.getQuantity() << endl;
+        }
+    }
+
     return 0;
 }
